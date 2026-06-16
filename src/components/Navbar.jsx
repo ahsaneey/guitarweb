9cf2ee525cf9da840c30c70d7efa81a8843c2673
ahsaneey <ahsanamfiroz@gmail.com>
2026-06-16 09:57:13 +0530

    updated navbar using responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-16 py-5">
+    <nav className="flex justify-between items-center px-6 md:px-16 py-5">
   <h1 className="font-bold text-xl">XPERIA</h1>
 
-  <ul className="flex gap-8 text-sm">
+  <ul className="flex gap-3 md:gap-8 text-sm md:text-sm">
     <li className="text-orange-500 border-b-2 border-orange-500">Home</li>
     <li>Parts</li>
     <li>Services</li>
